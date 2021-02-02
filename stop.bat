6cfd2775a19cc7f7590da4b05afd3546d2d5063a
SfireInc <thomoudard@gmail.com>
2021-02-02 17:10:17 +0100

    Travis CI

--- stop.bat
+++ stop.bat
@@ -1,8 +1,6 @@
 docker stop postgres_DevOps
-docker stop adminer_DevOps
 docker stop java_DevOps
 docker stop httpd_DevOps
 docker rm postgres_DevOps
-docker rm adminer_DevOps
 docker rm java_DevOps
 docker rm httpd_DevOps
