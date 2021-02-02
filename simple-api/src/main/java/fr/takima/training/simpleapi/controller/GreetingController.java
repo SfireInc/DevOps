d2fb799bceb1bdc38001d6d6e9047bde1aaa7c38
SfireInc <thomoudard@gmail.com>
2021-02-02 11:58:06 +0100

    BackEnd API

--- simple-api/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
@@ -1,10 +1,12 @@
 package fr.takima.training.simpleapi.controller;
 
 import org.springframework.web.bind.annotation.*;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
@@ -14,6 +16,7 @@ public class GreetingController {
     }
 
     class Greeting {
+
         private final long id;
         private final String content;
 
